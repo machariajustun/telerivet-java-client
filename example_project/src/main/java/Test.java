45702b8b26ad99a625c8d7157c2c87c24e159c4e
Justun Macharia Muriga <jmacharia@Justuns-MacBook-Pro.local>
2017-05-07 16:19:20 +0300

    updated the initial test

--- example_project/src/main/java/Test.java
+++ example_project/src/main/java/Test.java
@@ -1,74 +1,29 @@
-
 import com.telerivet.*;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
 
-public class Test {
-    
-    public static void main(String[] args) {
-                
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
-                
-        try
-        {
-            System.out.print("Enter your API key: ");
-            String apiKey = br.readLine().trim();   
-            TelerivetAPI tr = new TelerivetAPI(apiKey);
-            
-            APICursor<Project> cursor = tr.queryProjects();
-            
-            List<Project> projects = cursor.all();
-            
-            Project project;
-            
-            int numProjects = projects.size();
-            
-            if (numProjects > 1)
-            {                
-                for (int i = 0; i < numProjects; i++)
-                {
-                    System.out.println((i + 1) + ": " + projects.get(i).getName());
-                }
-                
-                System.out.print("Choose a project (1-"+numProjects + "): ");
-                String projectIndexStr = br.readLine();
-                int projectIndex = Integer.parseInt(projectIndexStr) - 1;
-                if (projectIndex < 0 || projectIndex >= numProjects)
-                {
-                    System.out.println("Invalid project index");
-                    return;
-                }
-                
-                project = projects.get(projectIndex);
-            }
-            else if (numProjects == 1)
-            {
-                project = projects.get(0);
-            }
-            else
-            {
-                System.out.println("You do not have any projects.");
-                return;
-            }
-            
-            System.out.print("Phone number to send message to: ");            
-            String toNumber = br.readLine().trim();
-            
-            System.out.print("Message to send: ");
-            String content = br.readLine();
-            
-            Message message = project.sendMessage(Util.options(
-                "to_number", toNumber,
-                "content", content
-            ));                        
-            
-            System.out.println("Message is " + message.getStatus() + ", message id=" + message.getId());            
-        }
-        catch (IOException ex)
-        {
-            throw new RuntimeException(ex);
-        }               
+public class Test 
+{    
+    public static void main(String[] args) throws IOException {
+   
+        String API_KEY = "up426oENT4Tl3byVHGlECkEbuXUdpGTi";  // from https://telerivet.com/api/keys
+        String PROJECT_ID = "PJ2e156d5ed55bf62e";
+
+        TelerivetAPI tr = new TelerivetAPI(API_KEY);
+
+        Project project = tr.initProjectById(PROJECT_ID);
+
+        // Import a contact
+        Contact contact = project.getOrCreateContact(Util.options(
+            "name", "John Smith",
+            "phone_number", "555-0001",
+            "vars", Util.options(
+                "birthdate", "1981-03-04",
+                "network", "Vodacom"
+            )
+        ));
+        
+        // Add a contact to a group    
+        Group group = project.getOrCreateGroup("Subscribers");
+        contact.addToGroup(group);
     }
 }
